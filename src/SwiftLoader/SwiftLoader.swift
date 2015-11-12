821ebd30915648c2499fe8cd5aa2cec9bcc22fba
Philippe Riegert <phyllipe68@gmail.com>
2015-11-11 19:24:16 -0500

    add the some MARK comments and refactor a little bit the code

--- src/SwiftLoader/SwiftLoader.swift
+++ src/SwiftLoader/SwiftLoader.swift
@@ -1,6 +1,6 @@
 //
-//  BSLoader.swift
-//  Brainstorage
+//  SwiftLoader.swift
+//  SwiftLoader
 //
 //  Created by Kirill Kunst on 07.02.15.
 //  Copyright (c) 2015 Kirill Kunst. All rights reserved.
@@ -11,24 +11,32 @@ import QuartzCore
 import CoreGraphics
 
 let loaderSpinnerMarginSide : CGFloat = 35.0
-let loaderSpinnerMarginTop : CGFloat = 20.0
-let loaderTitleMargin : CGFloat = 5.0
+let loaderSpinnerMarginTop  : CGFloat = 20.0
+let loaderTitleMargin       : CGFloat = 5.0
 
 public class SwiftLoader: UIView {
     
-    private var coverView : UIView?
-    private var titleLabel : UILabel?
+    /*
+     * MARK: - Private variables
+     */
+    
+    private var coverView   : UIView?
+    private var titleLabel  : UILabel?
     private var loadingView : SwiftLoadingView?
-    private var animated : Bool = true
-    private var canUpdated = false
-    private var title: String?
-    private var speed = 1
+    private var animated    : Bool = true
+    private var canUpdated  : Bool = false
+    private var title       : String?
+    private var speed       : Int = 1
     
     private var config = SwiftLoaderConfig() {
         didSet {
             self.loadingView?.config = config
         }
     }
+    
+    /*
+     * MARK: - Public variables
+     */
     
     override public var frame : CGRect {
         didSet {
@@ -43,19 +51,36 @@ public class SwiftLoader: UIView {
         return Singleton.instance
     }
     
-    public class func show(animated: Bool) {
-        self.show(nil, animated: animated, topMargin: 0)
+    /*
+     * MARK: - Constructor
+     */
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setup()
     }
     
-    public class func show(animated: Bool, topMargin: Int) {
-        self.show(nil, animated: animated, topMargin: topMargin)
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
     }
     
-    public class func show(title: String?, animated: Bool) {
-        self.show(title, animated: animated, topMargin: 0)
+    /*
+     * MARK: - Public methods
+     */
+    
+    public class func show(animated animated: Bool) {
+        self.show(animated: animated, title: nil, topMargin: 0)
     }
     
-    public class func show(title: String?, animated: Bool, topMargin: Int) {
+    public class func show(animated animated: Bool, title: String?) {
+        self.show(animated: animated, title: title, topMargin: 0)
+    }
+    
+    public class func show(animated animated: Bool, topMargin: Int) {
+        self.show(animated: animated, title: nil, topMargin: topMargin)
+    }
+    
+    public class func show(animated animated: Bool, title: String?, topMargin: Int) {
         let currentWindow : UIWindow = UIApplication.sharedApplication().keyWindow!
         
         let loader = SwiftLoader.sharedInstance
@@ -65,11 +90,11 @@ public class SwiftLoader: UIView {
         loader.update()
         
         let height : CGFloat = UIScreen.mainScreen().bounds.size.height
-        let width : CGFloat = UIScreen.mainScreen().bounds.size.width
+        let width  : CGFloat = UIScreen.mainScreen().bounds.size.width
         let center : CGPoint = CGPointMake(width / 2.0, height / 2.0 - CGFloat(topMargin))
         loader.center = center
         
-        if (loader.superview == nil) {
+        if loader.superview == nil {
             loader.coverView = UIView(frame: currentWindow.bounds)
             loader.coverView?.backgroundColor = loader.config.foregroundColor.colorWithAlphaComponent(loader.config.foregroundAlpha)
             
@@ -90,19 +115,14 @@ public class SwiftLoader: UIView {
         loader.frame = CGRectMake(0, 0, loader.config.size, loader.config.size)
     }
     
-    /**
-    Private methods
-    */
-    
-    private func setup() {
-        self.alpha = 0
-        self.update()
-    }
+    /*
+     * MARK: - Private methods
+     */
     
     private func start() {
         self.loadingView?.start()
         
-        if (self.animated) {
+        if self.animated {
             UIView.animateWithDuration(0.3, animations: { () -> Void in
                 self.alpha = 1
                 }, completion: { (finished) -> Void in
@@ -114,8 +134,7 @@ public class SwiftLoader: UIView {
     }
     
     private func stop() {
-        
-        if (self.animated) {
+        if self.animated {
             UIView.animateWithDuration(0.3, animations: { () -> Void in
                 self.alpha = 0
                 }, completion: { (finished) -> Void in
@@ -131,19 +150,24 @@ public class SwiftLoader: UIView {
         }
     }
     
+    private func setup() {
+        self.alpha = 0
+        self.update()
+    }
+    
     private func update() {
         self.backgroundColor = self.config.backgroundColor
         self.layer.cornerRadius = self.config.cornerRadius
         let loadingViewSize = self.frame.size.width - (loaderSpinnerMarginSide * 2)
         
-        if (self.loadingView == nil) {
+        if self.loadingView == nil {
             self.loadingView = SwiftLoadingView(frame: self.frameForSpinner())
             self.addSubview(self.loadingView!)
         } else {
             self.loadingView?.frame = self.frameForSpinner()
         }
         
-        if (self.titleLabel == nil) {
+        if self.titleLabel == nil {
             self.titleLabel = UILabel(frame: CGRectMake(loaderTitleMargin, loaderSpinnerMarginTop + loadingViewSize, self.frame.width - loaderTitleMargin*2, 42.0))
             self.addSubview(self.titleLabel!)
             self.titleLabel?.numberOfLines = 1
@@ -160,15 +184,14 @@ public class SwiftLoader: UIView {
         self.titleLabel?.hidden = self.title == nil
     }
     
-    func frameForSpinner() -> CGRect {
+    private func frameForSpinner() -> CGRect {
         let loadingViewSize = self.frame.size.width - (loaderSpinnerMarginSide * 2)
         
-        if (self.title == nil) {
+        if self.title == nil {
             let yOffset = (self.frame.size.height - loadingViewSize) / 2
             return CGRectMake(loaderSpinnerMarginSide, yOffset, loadingViewSize, loadingViewSize)
-        }
-        return CGRectMake(loaderSpinnerMarginSide, loaderSpinnerMarginTop, loadingViewSize, loadingViewSize)
-    }
+        } else {
+            return CGRectMake(loaderSpinnerMarginSide, loaderSpinnerMarginTop, loadingViewSize, loadingViewSize)
         }
     }
     
